d24b4a334a34d2887ca3321fea634ffb370e30ec
HamadaTamer <141588693+HamadaTamer@users.noreply.github.com>
2024-02-19 00:40:54 +0100

    Controlling a servo with a potentiometer

---
+++ servoWithPotentiometer.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo servo1;
+int servopin=11;
+int vres=A0;
+
+void setup()
+{
+  servo1.attach(servopin);
+  pinMode(vres, INPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  
+  int val=analogRead(vres);
+  int angle =map(val, 0,1023, 0,180);
+  servo1.write(angle);
+  Serial.println(angle);
+}
